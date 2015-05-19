ad6192aeaf1c4a6424f6fd42a8a51203813f3494
Kate890217 <yu_jie_lei@126.com>
2015-05-19 22:59:21 +0800

    update BaseDao

--- src/main/java/com/kate/app/dao/BaseDao.java
+++ src/main/java/com/kate/app/dao/BaseDao.java
@@ -13,13 +13,32 @@ public class BaseDao {
 	protected static String username = null;
 	protected static String password = null;
 	protected static Connection con = null;
+	
+	public BaseDao()
+	{
+		if(con == null){
+			try {
+	            url = "jdbc:mysql://101.200.174.253/gethouse";
+	
+	            username = "dboperator1";
+	            password = "gethouse";
+	            con = DriverManager.getConnection(url, username, password);
+	        } catch (Exception e) {
+	           System.out.print("DriverManager.getConnection faild");
+	           System.out.println(e.toString());
+	        }
+		}else{
+			
+		}
+		
+	}
 	static {
 		try{   
-		    //����MySql������   
+		    //锟斤拷锟斤拷MySql锟斤拷锟斤拷锟斤拷   
 		    Class.forName("com.mysql.jdbc.Driver") ;   
 		}
 		catch(ClassNotFoundException e){   
-		    System.out.println("�Ҳ���������� ��������ʧ�ܣ�");   
+		    System.out.println("锟揭诧拷锟斤拷锟斤拷锟斤拷锟斤拷锟�锟斤拷锟斤拷锟斤拷锟斤拷失锟杰ｏ拷");   
 		    e.printStackTrace() ;   
 		}   
 		try {
